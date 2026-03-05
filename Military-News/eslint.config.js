211cd3ac6dfc4f732aef0bfa3717ad5bc5973610
Anmolsinh Chudasama <anmolsinh.chudasama@hitechdigital.com>
2026-03-05 15:08:35 +0530

    Final husky commit

--- Military-News/eslint.config.js
+++ Military-News/eslint.config.js
@@ -1,18 +1,18 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  { ignores: ['dist', 'node_modules'] },
+  { ignores: ["dist", "node_modules"] },
   js.configs.recommended,
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+      ecmaVersion: "latest",
+      sourceType: "module",
       globals: {
         ...globals.browser,
         ...globals.node,
@@ -25,29 +25,29 @@ export default [
     },
     plugins: {
       react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
       ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
+      ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
-      'react/prop-types': 'off',
-      
+      "react/prop-types": "off",
+      "react/display-name": "off",
       // CRITICAL: These rules force the commit to fail if there are real errors
-      'no-unused-vars': 'error', 
-      'no-undef': 'error'        
+      "no-unused-vars": "error",
+      "no-undef": "error",
     },
     settings: {
       react: {
-        version: 'detect',
+        version: "detect",
       },
     },
   },
   // This must be last. It turns off ESLint formatting rules so Prettier can do its job without conflicts
-  eslintConfigPrettier, 
+  eslintConfigPrettier,
 ];
